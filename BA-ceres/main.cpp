679c6b415aef8f942959dabe7a836a12607084d2
Weijun-Lin <weijun-lin@foxmail.com>
2021-11-27 21:12:38 +0800

    feat[A]:BA-ceres 添加 EPNP 初始化

--- BA-ceres/main.cpp
+++ BA-ceres/main.cpp
@@ -17,6 +17,8 @@
 #include <iostream>
 #include <Eigen/Geometry>
 #include <opencv2/core.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core/eigen.hpp>
 #include "utils.h"
 #include "DataGenerator.h"
 #include "BA.h"
@@ -53,14 +55,33 @@ int main(int argv, char **args) {
         p(2) += rng.gaussian(sigma);
     }
 
+    // 这里使用 EPNP 估计初始位姿（也可以使用其它的）
+    // 首先构造 OpenCV 形式的输入
+    cv::Mat r_cv, t_cv, K_cv, R_cv;
+    cv::eigen2cv(K, K_cv);
+    std::vector<cv::Point3d> pts3d_cv;
+    std::vector<cv::Point2d> pts2d_cv;
+    for(int i = 0;i < nums;i++) {
+        pts3d_cv.push_back(cv::Point3d(pts3d[i](0), pts3d[i](1), pts3d[i](2)));
+        pts2d_cv.push_back(cv::Point2d(pts2d[i](0), pts2d[i](1)));
+    }
+    // EPNP 求解
+    cv::solvePnP(pts3d_cv, pts2d_cv, K_cv, cv::Mat(), r_cv, t_cv, false, cv::SOLVEPNP_EPNP);
+    cv::Rodrigues(r_cv, R_cv);
+
+    std::cout.precision(5);
+    std::cout << "真实位姿：\n旋转矩阵：\n" 
+        << T.topLeftCorner(3,3) << "\n平移向量：" << t.transpose() << std::endl;
+
     // 问题求解
     BA ba(pts3d, pts2d, K);
     // 估计的位姿 非线性优化需要一个很好的初值，所以这里任意初值可能结果不是最优的
-    // 使用下面注释的可以达到最优，当然 R，t可以加入噪声干扰一下，效果也不错
-    // Eigen::Matrix3d R_ = R;
-    // Eigen::Vector3d t_ = t;
+    // 所以使用 EPNP 估计的初始值
     Eigen::Matrix3d R_ = Eigen::Matrix3d::Identity();
     Eigen::Vector3d t_;
+    cv::cv2eigen(t_cv, t_);
+    cv::cv2eigen(R_cv, R_);
+    std::cout << "EPNP 估计初始位姿：\n旋转矩阵：\n" << R_ << "\n平移向量：" << t_.transpose() << std::endl;
     // 这里连带空间点一起估计
     Points3d pts3d_ = pts3d;
     // 是否使用舒尔补求解 使用舒尔补在大量点的时候加速明显
@@ -68,8 +89,7 @@ int main(int argv, char **args) {
     ba.solve(R_, t_, pts3d_, isSchur);
 
     // 打印结果
-    std::cout << "真实位姿：\n" << T.topLeftCorner(3,3) << "\n" << t << std::endl;
-    std::cout << "估计位姿：\n" << R_ << "\n" << t_ << std::endl;
+    std::cout << "BA 估计位姿：\n旋转矩阵：\n" << R_ << "\n平移向量：" << t_.transpose() << std::endl;
     // 估计的空间点就不打印了
 
     return 0;
